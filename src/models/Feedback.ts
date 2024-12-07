44d22f441f203377ade7e327153c77f61d00fb98
MiraBellierr <75848@siswa.unimas.my>
2024-12-07 14:41:24 +0800

    Remove unnecessary files

--- src/models/Feedback.ts
+++
@@ -1,9 +0,0 @@
-export class Feedback {
-	constructor(
-		public feedbackId: string,
-		public complaintId: string,
-		public rating: number,
-		public comments: string,
-		public dateSubmitted: Date = new Date()
-	) {}
-}
--- src/models/User.ts
+++
@@ -1,9 +0,0 @@
-export abstract class User {
-	constructor(
-		public userId: string,
-		public password: string,
-		public name: string,
-		public email: string,
-		public role: string // "student" or "admin"
-	) {}
-}
